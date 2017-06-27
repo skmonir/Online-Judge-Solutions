ac4a7cd93a26663ec904c9ec0ddd86354f1f7ea2
Sheikh Monir <skmnrcse@gmail.com>
2017-06-27 14:52:01 +0600

    Update 1004 - Monkey Banana Problem.cpp

--- LightOJ/Volume-10/1004 - Monkey Banana Problem.cpp
+++ LightOJ/Volume-10/1004 - Monkey Banana Problem.cpp
@@ -1,62 +1,45 @@
 #include <bits/stdc++.h>
- 
+
 using namespace std;
- 
+
 #define endl           '\n'
-#define LL             long long
-#define ULL            unsigned long long
 #define all(x)         x.begin(), x.end()
- 
-const LL MOD = 1000000007;
-const LL LINF = 0x3f3f3f3f3f3f3f3fLL;
-const int INF = 0x3f3f3f3f;
-const int N = 2e2 + 5;
- 
-LL dp[N][N];
-int n, sz[N];
-vector <LL> a[N];
- 
-void init() {
-    for (int i = 0; i < 2 * N; i++) {
-        a[i].clear();
-        sz[i] = 0;
-    }
-    memset(dp, -1, sizeof dp);
-}
- 
-inline bool invalid(int x, int y) {
-    return x > 2 * n || y >= sz[x] || !a[x][y];
-}
- 
-LL DP(int x, int y) {
-    if (invalid(x, y)) return 0;
-    LL &res = dp[x][y];
-    if (~res) return res;
-    res = max(a[x][y] + DP(x + 1, y), a[x][y] + DP(x + 1, y + 1));
-    return res;
-}
- 
+#define rall(x)        x.rbegin(), x.rend()
+#define mem(x, y)      memset(x, y, sizeof x)
+
+template <typename T> T gcd(T a, T b) {return !b ? a : gcd(b, a % b);}
+template <typename T> inline void umin(T &a, T b) {a = a < b ? a : b;}
+template <typename T> inline void umax(T &a, T b) {a = a > b ? a : b;}
+
+const int N = (int) 1e5 + 5;
+const int INF = (int) 1e9 + 7;
+const long long MOD = (long long) 1e9 + 7;
+
+int a[223][223], dp[223][223];
+
 int main() {
     int tc, ks = 0;
     scanf("%d", &tc);
     while (tc--) {
-        init();
-        scanf("%d", &n);
-        for (int i = 1; i <= n; i++) {
+        mem(a, 0), mem(dp, 0);
+        int n, m; scanf("%d", &m);
+        n = 2 * m - 1;
+        for (int i = 1; i <= m; i++) {
             for (int j = 1; j <= i; j++) {
-                LL x; scanf("%lld", &x);
-                a[i - 1].push_back(x);
+                scanf("%d", &a[i][j]);
             }
         }
-        for (int i = 1; i <= n - 1; i++) {
-            for (int j = 1; j <= i; j++) a[n + i - 1].push_back(0);
-            for (int j = 1; j <= n - i; j++) {
-                LL x; scanf("%lld", &x);
-                a[n + i - 1].push_back(x);
+        for (int i = 1; i < m; i++) {
+            for (int j = 1; j <= m - i; j++) {
+                scanf("%d", &a[i + m][j + i]);
             }
         }
-        for (int i = 0; i < 2 * n - 1; i++) sz[i] = a[i].size();
-        printf("Case %d: %lld\n", ++ks, DP(0, 0));
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= m; j++) {
+                dp[i][j] = a[i][j] + max(dp[i - 1][j], dp[i - 1][j - 1]);
+            }
+        }
+        printf("Case %d: %d\n", ++ks, dp[n][m]);
     }
     return 0;
 }
